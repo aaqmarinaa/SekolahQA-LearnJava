d55a2dd2f89b892f66aa97442e73ff83c9db1303
aaqmarinaa <almasaqmarina@gmail.com>
2020-10-31 09:47:39 +0700

    init

---
+++ cilsy/src/main/java/com/cilsy/App.java
@@ -0,0 +1,22 @@
+package com.cilsy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( getMyName() );
+        System.out.println( getMyNumber() );
+    }
+
+    public static String getMyName(){
+        return "Hello my name is Almas";
+    }
+
+    public static Integer getMyNumber(){
+        return 1234567;
+    }
+}
