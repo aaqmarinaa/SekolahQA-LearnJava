b137c31b0f85ed84250b024938aee39e844066a2
aaqmarinaa <almasaqmarina@gmail.com>
2020-11-13 13:54:52 +0700

    nested if

---
+++ cilsy/src/main/java/com/cilsy/NestedIf.java
@@ -0,0 +1,78 @@
+package com.cilsy;
+
+import java.util.ArrayList;
+
+
+public class NestedIf {
+	public static void main(String[] args) {
+//		if ("a" == "A" && 5 > 10 || false == true && "AA".equalsIgnoreCase("aa")) {
+//			if("apapun".length() == 6) {
+//				if () {
+//					
+//				}
+//			} else {
+//				
+//			}
+//		} else if ( ) {
+//			
+//		} else if () {
+//			
+//		} else {
+//			
+//		}
+//		int angka = 5;
+//		
+//		switch (angka) {
+//		case 
+//		}
+//		int[] angka = new int[11];
+//		angka[1] = 16;
+//		System.out.println(angka[1]);
+//		
+//		String[] huruf = new String[13];
+//		huruf[10] = "fafa";
+//		System.out.println(huruf[10]);
+		
+//		String[] aaa = new String[13];
+//		aaa[10] = "";
+//		System.out.println(aaa[4]);
+//		if(huruf[4].equals("shshs")) {
+//			System.out.println("masuk");
+//		}
+		
+		java.util.List<Object> arrObj = new ArrayList();
+		arrObj.add(2);
+		arrObj.add(true);
+		arrObj.add("true");
+//		System.out.println(arrObj.get(2));
+		
+		int counter = 0;
+		for (int i = 0; i < 100; i++) {
+			for (int j=0; j < 10; j+=2) {
+				counter++;
+				if (i % 2 == 0) {
+					System.out.println(j);
+				} else {
+					System.out.println(i);
+				}
+			}
+		}
+		System.out.println(counter);
+		
+//		for (Object obj: arrObj) {
+//			System.out.println(obj.toString().getClass());
+//		}
+//		
+//		for (int i=0; i<arrObj.size(); i++) {
+//			System.out.println(arrObj.get(i).getClass());
+//		}
+		
+		//List Data Diri
+		//inputan data dari user
+		// nama, masukkan array list, alamat
+		
+//		for (int i=0; i <=10; i+=3) {
+//			System.out.println(i);
+//		}
+	}
+}
