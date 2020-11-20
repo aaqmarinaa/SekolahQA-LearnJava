6e0b708d588b1263731f0e40e16d75bb6c017198
aaqmarinaa <almasaqmarina@gmail.com>
2020-11-20 16:48:52 +0700

    OOP

---
+++ cilsy/src/main/java/dasar/NestedClass.java
@@ -0,0 +1,17 @@
+package dasar;
+
+class Outer {
+	// Simple nested inner class
+	class Inner {
+		public void show() {
+			System.out.println("In a nested class method");
+			}
+		}
+	}
+	
+class Main {
+	public static void main(String[] args) {
+		Outer.Inner in = new Outer().new Inner();
+		in.show();
+		}
+	}
---
+++ cilsy/src/main/java/hpbaru/Phone.java
@@ -0,0 +1,10 @@
+package hpbaru;
+
+public class Phone {
+	int MAX_VOLUME = 100;
+	int MIN_VOLUME = 0;
+	void powerOn();
+	void powerOff();
+	void volumeUp();
+	void volumeDown();
+}
